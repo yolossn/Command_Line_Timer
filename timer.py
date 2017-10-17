3c095f5c70867f5d307197e740f4af4356e44c00
yolossn <nssvlr@gmail.com>
2017-10-17 12:49:47 +0530

    make arguments compulsory and print statements pretty

--- timer.py
+++ timer.py
@@ -2,18 +2,32 @@ import time,sys,subprocess,argparse
 
 def timer(sec):
 	for i in range(sec,0,-1):
+		m,s=divmod(i,60)
+		h,m=divmod(m,60)
 		if(sec>3600):
-			print("\rh:{} m:{} s:{} ".format(i%3600,i%60,i),end="")
+			print("\rh:{} m:{} s:{} ".format(h,m,s),end="")
 		else:
-			print("\rm:{} s:{} ".format(i%60,i))
+			print("\rm:{} s:{} ".format(m,s),end="")
 		time.sleep(1)
+	play() #function call to play alert sound
 
-def play(fname="beep_beep_beep_alarm.mp3"):
-	subprocess.call(["ffplay","-nodisp","-autoexit",fname])
+def play(fname="beep_beep_beep_alarm.mp3"): #function to play timer alert sound
+	subprocess.call(["ffplay","-nodisp","-hide_banner","-autoexit",fname])
 
 parser=argparse.ArgumentParser()
-parser.add_argument("-hs","--hours",type=int,help="hours in numbers")
-parser.add_argument("-m","--minutes",help="minutes in numbers",type=int)
-parser.add_argument("-s","--seconds",help="seconds in numbers",type=int)
+parser.add_argument("hours",help="hours in numbers",type=int) #compulsory argument of type int for hours
+parser.add_argument("minutes",help="minutes in numbers",type=int) #compulsory argument of type int for mins 
+parser.add_argument("seconds",help="seconds in numbers",type=int) #compulsory argument of type int for secs
 args=parser.parse_args()
-print(len(vars(args)))
+secs=0
+if args.seconds>=60: #convert secs >60 to mins and secs
+	args.seconds%=60
+	args.minutes+=args.seconds//60
+if args.minutes>=60: #convert mins >60 to hrs and mins
+	args.minutes%=60
+	args.hours+=args.minutes//60
+secs+=args.hours*3600 #convert hrs to sec
+secs+=args.minutes*60 #convert mins to sec
+secs+=args.seconds #add secs
+#print(secs)
+timer(secs)
