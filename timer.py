298b19b5d9ac4d0f41346ec61da394417e5535e6
yolossn <nssvlr@gmail.com>
2017-10-17 12:07:28 +0530

    get commandline arguments using argparse

--- timer.py
+++ timer.py
@@ -12,6 +12,8 @@ def play(fname="beep_beep_beep_alarm.mp3"):
 	subprocess.call(["ffplay","-nodisp","-autoexit",fname])
 
 parser=argparse.ArgumentParser()
-parser.add_argument("-i",help="in")
+parser.add_argument("-hs","--hours",type=int,help="hours in numbers")
+parser.add_argument("-m","--minutes",help="minutes in numbers",type=int)
+parser.add_argument("-s","--seconds",help="seconds in numbers",type=int)
 args=parser.parse_args()
-print(args)
+print(len(vars(args)))
