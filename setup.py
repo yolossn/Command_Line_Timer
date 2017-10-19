57142ccff4c87875c215f28db253d1c73da8ed7d
yolossn <nssvlr@gmail.com>
2017-10-19 12:27:31 +0530

    setup.py for commandline command

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name="timer",
+	description="terminal timer",
+	author="yolossn",
+	author_email="nssvlr@gmail.com",
+	license="MIT",
+	keywords=["timer"],
+	scripts=["timer"]
+)
